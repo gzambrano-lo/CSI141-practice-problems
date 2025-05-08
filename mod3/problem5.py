80e65dd95f400d4ab633f16161d7282b37f253e9
Guadalupe Zambrano <gzamlo98@gmail.com>
2025-05-08 14:13:47 -0700

    Update problem5.py

--- mod3/problem5.py
+++ mod3/problem5.py
@@ -1,1 +1,4 @@
+Print 3 words with a | character (called a pipe) in between them. Use the appropriate keyword argument in print() to do so.
 
+# Print 3 words with a | character (called a pipe) in between them. Use the appropriate keyword argument in print() to do so.
+print("bologna", "ham", "turkey", sep="|")
