3673c4247a0714b95515250d982321a3943603f0
Guadalupe Zambrano <gzamlo98@gmail.com>
2025-05-08 14:07:28 -0700

    Update problem3.py

--- mod3/problem3.py
+++ mod3/problem3.py
@@ -1,2 +1,8 @@
 Prompt the user for a sentence and a word to try to find in that sentence. Have the program print out whether the word was found in the sentence. (i.e. True or False)
-Solution
+
+# Prompt the user for a sentence 
+sentence = input("Enter a sentence: ")
+# Prompt the user for a word to find 
+word = input ("Enter a word to find in the sentence: ") 
+# Check if the word is in the sentence and print the result
+print(word in sentence)
