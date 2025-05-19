21c61e1fa4e06ab2d505db2b1a829108fec0693c
Guadalupe Zambrano <gzamlo98@gmail.com>
2025-05-18 23:58:38 -0700

    Create Problem_3.py

---
+++ Mod3/Problem_3.py
@@ -0,0 +1,6 @@
+# Prompt the user for a sentence
+sentence = input("Enter a sentence: ")
+# Prompt the user for a word to find
+word = input ("Enter a word to find in the sentence: ")
+# Check if the word is in the sentence and print the result
+print(word in sentence)
