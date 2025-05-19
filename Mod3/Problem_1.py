c26958b6d197b6c8e669daaa88544281409eae6c
Guadalupe Zambrano <gzamlo98@gmail.com>
2025-05-18 23:57:25 -0700

    Create Problem_1.py

---
+++ Mod3/Problem_1.py
@@ -0,0 +1,7 @@
+# Prompt user for a word
+word = input("Enter a Word: ")
+# Prompt the user for how many times to repeat a word
+times = int(input("How many times would you like to repeat the word?"))
+# Print the word the specified number of times
+for i in range(times):
+    print(word)
